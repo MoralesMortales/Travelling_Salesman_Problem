978789d35468b3c52391cce0e38d0773916a3b3f
CarlosLup <moralesyaguilera.com>
2024-05-05 22:49:56 -0400

    esta version tiene la asignacion de todos los valores

--- 1.cpp
+++ 1.cpp
@@ -5,9 +5,15 @@
 class problema {
  private:
   int n;
+  int cities[100][100][100];
+  int nro_dimensions;
 
  public:
   problema() {}
+
+  problema(int n_city, int n_dimensions);
+
+  void insert_values(int limit, int d1[100][100][100]);
 
   float distancia(int city_1, int city_2);
 
@@ -15,15 +21,12 @@ class problema {
 
   void push_n(int the_n);
 
-  void insert_coordenadas(int in_x[], int in_y[]);
-
   int getn();
 
   void show_cities();
 };
 
 class tour {
-
  private:
   problema problem_tour;
   int configuracion[1][101];
@@ -45,9 +48,137 @@ class tour {
 
 using namespace std;
 
+// TODO:
+
 int main() {
-  
-	int n;
+  srand(time(NULL));
+
+  int n_cities, n_dimensions;
+  int limit_coords = 100;
+
+  int dimensions[100][100][100];  // limite de coordenadas
+
+  cout << "cantidad de ciudades: ";
+  cin >> n_cities;
+  cout << "cantidad de dimensiones (1-3)";
+  cin >> n_dimensions;
+
+  // FIXME: esto rellena todos los lugares, no es lo que queremos, queremos
+  // solamente la cantidad de ciudades
+
+  // for (int f = 0; f < n_cities; f++) {
+  //   switch (n_dimensions) {
+  //     case 1:
+  //       for (int f = 0; f < limit_coords; f++) {
+  //         dimensions[f][0][0] = rand() % 100;
+  //       }
+
+  //       break;
+  //     case 2:
+
+  //       for (int f = 0; f < limit_coords; f++) {
+  //         for (int s = 0; s < limit_coords; s++) {
+  //           dimensions[f][s][0] = rand() % 100;
+  //         }
+  //       }
+
+  //       break;
+
+  //     case 3:
+  //       for (int f = 0; f < limit_coords; f++) {
+  //         for (int s = 0; s < limit_coords; s++) {
+  //           for (int t = 0; t < limit_coords; t++) {
+  //             dimensions[f][s][t] = rand() % 100;
+  //           }
+  //         }
+  //       }
+  //       break;
+
+  //     default:
+  //       cout << "Error 101 \n";
+  //       break;
+  //   }
+  // }
+
+
+
+  problema Pais(n_cities, n_dimensions);
+
+  Pais.insert_values(limit_coords, dimensions);
+
+  Pais.show_cities();
 
   return 0;
+}
+
+problema::problema(int n_city, int n_dimensions) {
+  n = n_city;
+  nro_dimensions = n_dimensions;
+}
+
+void problema::insert_values(int limit, int d1[100][100][100]) {
+  for (int f = 0; f < n; f++) {
+    switch (nro_dimensions) {
+      case 1:
+        for (int f = 0; f < limit; f++) {
+          cities[f][0][0] = d1[f][0][0];
+        }
+
+        break;
+      case 2:
+
+        for (int f = 0; f < limit; f++) {
+          for (int s = 0; s < limit; s++) {
+            cities[f][s][0] = d1[f][s][0];
+          }
+        }
+
+        break;
+
+      case 3:
+        for (int f = 0; f < limit; f++) {
+          for (int s = 0; s < limit; s++) {
+            for (int t = 0; t < limit; t++) {
+              cities[f][s][t] = d1[f][s][t];
+            }
+          }
+        }
+        break;
+    }
+  }
+}
+
+void problema::show_cities() {
+  switch (nro_dimensions) {
+    case 1:
+      for (int f = 0; f < n; f++) {
+        cout << "En " << f << ". x:" << f << ". =" << cities[f][0][0] << "\n";
+      }
+      cout << "\n";
+
+      break;
+    case 2:
+
+      for (int f = 0; f < n; f++) {
+        for (int s = 0; s < n; s++) {
+          cout << "En x:" << f << ". y:" << s << ". =" << cities[f][s][0]
+               << "\n";
+        }
+      }
+      cout << "\n";
+      break;
+
+    case 3:
+      for (int f = 0; f < n; f++) {
+        for (int s = 0; s < n; s++) {
+          for (int t = 0; t < n; t++) {
+            cout << "En x:" << f << ". y:" << s << ". z:" << t
+                 << ". =" << cities[f][s][t] << "\n";
+          }
+        }
+      }
+      cout << "\n";
+
+      break;
+  }
 }
